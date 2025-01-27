301ed5143dbac5128baff4b95d82defd7971ad4c
INDIAN CODER <98512400+Adityaranjan0918@users.noreply.github.com>
2025-01-27 15:13:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ API_HASH = getenv("API_HASH", None)
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 
 #port
-PORT = environ.get("PORT", "8080")
+PORT = getenv("PORT", "8080")
 
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
