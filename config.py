343d3e27620c92ba69c6ef7d147059b476805c65
INDIAN CODER <98512400+Adityaranjan0918@users.noreply.github.com>
2025-01-27 15:01:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,6 +14,9 @@ API_HASH = getenv("API_HASH", None)
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 
+#port
+PORT = environ.get("PORT", "8080")
+
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", None)
